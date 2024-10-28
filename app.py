ee69b283d842af222864184a2bade3a4ff580966
单子亨 <brianshan974@gmail.com>
2024-10-28 15:41:33 +0000

    Modified the format

--- app.py
+++ app.py
@@ -121,14 +121,14 @@ def process_query(query: str):
             for number in numbers:
                 if is_square_and_cube(number):
                     s_c.append(number)
-            return ",".join(s_c)
+            return ", ".join(s_c)
         if "prime" in query:
             numbers = [int(i) for i in query.split(": ")[1][:-1].split(", ")]
             primes = []
             for number in numbers:
                 if is_prime(number):
                     primes.append(number)
-            return ",".join(primes)
+            return ", ".join(primes)
         else:
             return "Unknown"
     else:
