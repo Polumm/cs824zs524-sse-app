0f3683a1148cbd73b73c2802f8006a2f5bb43a9f
单子亨 <brianshan974@gmail.com>
2024-11-04 15:16:40 +0000

    Added a personal token to increase rate limit

--- app.py
+++ app.py
@@ -225,7 +225,7 @@ def repos():
         #     f"Error fetching repositories for user"
         #     f" {username}: {response.status_code}"
         # )
-        return f"{status_code=}, {response.text=}, {response.request.headers=}"
+        return f"{status_code=}, {response.text=}"
 
 
 if __name__ == "__main__":
